6407daafcf401a86415659fcb81289e08e4a8e5a
J. David Giese <johndgiese@gmail.com>
2015-07-26 16:33:22 -0400

    Merge in VHDL parser + update makefile

--- Makefile
+++ Makefile
@@ -3,18 +3,33 @@ SHELL = /bin/bash
 
 IN_SANDBOX = cabal exec -- 
 
-Main: Main.hs .cabal-sandbox
-	$(IN_SANDBOX) ghc --make Main.hs
+%.o: %.hs
+	$(IN_SANDBOX) ghc $<
+
+all: WebServer VhdlParser
+
+WebServer: WebServer.hs .cabal-sandbox
+	$(IN_SANDBOX) ghc --make $<
+
+VhdlParser: VhdlEntityTree.hs VhdlParser.hs Tokens.hs .cabal-sandbox
+	$(IN_SANDBOX) ghc --make VhdlParser.hs
+
+Tokens.hs: Tokens.x .cabal-sandbox
+	$(IN_SANDBOX) alex Tokens.x
 
 .cabal-sandbox: cabal.config
 	cabal sandbox init
 	cabal install scotty
+	cabal install alex
+	cabal install json
 
 .PHONY: clean
 clean:
 	rm *.o
 	rm *.hi
-	rm -f Main
+	rm -f WebServer
+	rm -f VhdlParser
+	rm -f Tokens.hs
 
 .PHONY: depclean
 depclean:
---
+++ VhdlParser.hs
@@ -0,0 +1,39 @@
+import Data.Maybe
+import Data.Tree
+import Text.JSON
+import VhdlEntityTree
+import System.Directory
+import System.Environment
+import qualified Data.List as List
+import qualified Tokens as Tokens
+
+type Command = String
+
+main = do
+  args <- getArgs
+  let cmds =  map parseCmdLineArg args
+      vhdlFolderPath = justLookup "path" cmds
+      topLevelEntity = justLookup "topLevel" cmds
+  filePaths <- getDirectoryContents vhdlFolderPath
+  let vhdlFilePaths = zipWith (++) (repeat vhdlFolderPath) $ filter (List.isInfixOf ".vhd") filePaths
+  xs <- sequence $ map readFile vhdlFilePaths
+  let entityLists = map (findEntities . Tokens.alexScanTokens) $ xs
+      entityAssoc = map (\(y:ys)->(y,ys)) $ filter (not . null) $ entityLists
+  putStrLn $ drawTree $ buildTopLevelTree topLevelEntity entityAssoc
+  writeFile (topLevelEntity ++ ".json") $ encode . toJSObject $ entityAssoc
+
+findEntities :: [String] -> [String]
+findEntities [] = []
+findEntities (x:xs) = filter (/=[]) $ (if x == "entity" then head xs else []) : findEntities xs
+
+parseCmdLineArg :: String ->(Command,String)
+parseCmdLineArg a
+  | List.isPrefixOf "--path=" a = ("path",filter (/='"') arg)
+  | List.isPrefixOf "--topLevel=" a = ("topLevel",arg)
+  | otherwise = ("Invalid Command","")
+  where
+    arg = tail . snd $ List.break (=='=') a
+
+justLookup ::(Eq a) => a -> [(a,k)] -> k
+justLookup v aList = fromJust $ List.lookup v aList
+
