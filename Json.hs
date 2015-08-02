c8582417f030b45e83eb7d959e145e7348e6f693
J. David Giese <johndgiese@gmail.com>
2015-08-02 13:00:22 -0400

    Make the `Tree` type JSON-encodeable

---
+++ Json.hs
@@ -0,0 +1,15 @@
+import Text.JSON
+import Data.Tree
+import Data.Maybe
+
+instance JSON a => JSON (Tree a) where
+    showJSON (Node p c) = JSObject $ toJSObject [("value", showJSON p), ("children", showJSONs c)] 
+    readJSON (JSObject o)
+        | isNothing value || isNothing children = Error "Unable to read Tree"
+        | otherwise = Node <$> (readJSON $ fromJust value) <*> (readJSONs $ fromJust children)
+        where
+            value = "value" `lookup` oAssocList
+            children = "children" `lookup` oAssocList
+            oAssocList = fromJSObject o
+
+    readJSON _ = Error "Unable to read Tree"
